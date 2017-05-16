98e50b2ad6cb8742a4bfcd43247bcddcbc11bf13
cxphong <caoxuanphong.khtn@gmail.com>
2017-05-16 14:36:27 +0700

    Update gstreamer-build.sh

--- gstreamer-build.sh
+++ gstreamer-build.sh
@@ -95,9 +95,8 @@ git checkout tags/$TAG
 git clean -f -d -x
 sudo make uninstall || true
 # some extra flags on rpi
-./autogen.sh CFLAGS="-I/opt/vc/include \
--I/opt/vc/include/interface/vcos/pthreads \
--I/opt/vc/include/interface/vmcs_host/linux/" LDFLAGS="-L/opt/vc/lib" \
+./autogen.sh CFLAGS="-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux/" \
+LDFLAGS="-L/opt/vc/lib" \
 --disable-gtk-doc --disable-opengl --enable-gles2 --enable-egl --disable-glx \
 --disable-x11 --disable-wayland --enable-dispmanx \
 --with-gles2-module-name=/opt/vc/lib/libGLESv2.so \
