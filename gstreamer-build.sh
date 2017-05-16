73319dba1fbcddb8dc9d000e929c3a6bd5e2632a
cxphong <caoxuanphong.khtn@gmail.com>
2017-05-16 18:43:46 +0700

    Update gstreamer-build.sh

--- gstreamer-build.sh
+++ gstreamer-build.sh
@@ -79,6 +79,20 @@ make -j4
 sudo make install
 cd ..
 
+cd gst-plugins-bad
+# some extra flags on rpi
+./autogen.sh --disable-gtk-doc
+./configure CFLAGS="-I/opt/vc/include \
+-I/opt/vc/include/interface/vcos/pthreads \
+-I/opt/vc/include/interface/vmcs_host/linux/" LDFLAGS="-L/opt/vc/lib" \
+--disable-gtk-doc --disable-opengl --enable-gles2 --enable-egl --disable-glx \
+--disable-x11 --disable-wayland --enable-dispmanx \
+--with-gles2-module-name=/opt/vc/lib/libGLESv2.so \
+--with-egl-module-name=/opt/vc/lib/libEGL.so
+make -j4
+sudo make install
+cd ..
+
 # omx support
 cd gst-omx
 export LDFLAGS='-L/opt/vc/lib' \
