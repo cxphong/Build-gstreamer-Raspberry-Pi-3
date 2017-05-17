3062fd9c4f9d70ebe1a9af7c3f1e482a9cd0c30c
caoxuanphong <caoxuanphong.khtn@gmail.com>
2017-05-17 18:58:50 +0700

    Update gstreamer-build.sh

--- gstreamer-build.sh
+++ gstreamer-build.sh
@@ -74,12 +74,14 @@ cd ..
 cd gst-plugins-bad
 # some extra flags on rpi
 ./autogen.sh --disable-gtk-doc
-./configure CFLAGS='-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux/' LDFLAGS='-L/opt/vc/lib' \
+export CFLAGS='-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux/'
+export LDFLAGS='-L/opt/vc/lib'
+./configure CFLAGS LDFLAGS\
 --disable-gtk-doc --disable-opengl --enable-gles2 --enable-egl --disable-glx \
 --disable-x11 --disable-wayland --enable-dispmanx \
 --with-gles2-module-name=/opt/vc/lib/libGLESv2.so \
 --with-egl-module-name=/opt/vc/lib/libEGL.so
-make -j4
+make CFLAGS+='-Wno-error -Wno-redundant-decls' LDFLAGS+='-L/opt/vc/lib' -j4
 sudo make install
 cd ..
 
